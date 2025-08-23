abe72de6faf17e4c0c74fff56b17d0262737882d
yusha-yazici <yushayazici27@gmail.com>
2025-08-23 13:46:22 +0900

    練習問題2/7days

---
+++ prob1_4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    printf("%d + %d + %d = %d\n",1,2,3,1+2+3);
+    return 0;
+}
---
+++ prob2_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    int a,b;								
+	printf("長方形の幅: ");						
+	scanf("%d",&a);						
+	printf("長方形の高さ: ");						
+	scanf("%d",&b);	
+	printf("長方形の面積は，%dm2です \n",a * b);		
+    return 0;
+}
